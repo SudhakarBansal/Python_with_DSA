3c8f6833b555a8e161f39a3a220966e92c257d88
Sudhakar Bansal <111216638+SudhakarBansal@users.noreply.github.com>
2024-11-25 15:34:09 +0530

    Inverted Pyramid

---
+++ 01_python_basics/07_inverted_pyramid.py
@@ -0,0 +1,21 @@
+# Inverted Pyramid
+
+def generate_inverted_pyramid(n):
+    """
+    Function to return an inverted pyramid pattern of '*' of side n as a list of strings.
+    
+    Parameters:
+    n (int): The number of rows in the inverted pyramid.
+    
+    Returns:
+    list: A list of strings where each string represents a row of the inverted pyramid.
+    """
+    # Your code here
+    inverted_pyramid =[]
+    for i in range(n,0,-1):
+        stars = ((2*i)-1) * "*"
+        spaces = (n-i)*" "
+        inverted_pyramid.append(spaces+stars+spaces)
+    return inverted_pyramid
+    
+print(generate_inverted_pyramid(5))
