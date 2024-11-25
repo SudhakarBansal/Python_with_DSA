ba20c5886fade4ff97982381a63001aea78b8d4f
Sudhakar Bansal <111216638+SudhakarBansal@users.noreply.github.com>
2024-11-25 12:44:54 +0530

    generate rectangle pattern

---
+++ 01_python_basics/03_generate_rectangle.py
@@ -0,0 +1,19 @@
+# Rectangle Pattern
+
+
+def generate_rectangle(n, m):
+    """
+    Function to return a rectangle pattern of '*' with length n and breadth m as a list of strings.
+    
+    Parameters:
+    n (int): The number of rows in the rectangle.
+    m (int): The number of columns in the rectangle.
+    
+    Returns:
+    list: A list of strings where each string represents a row of the rectangle pattern.
+    """
+    # Your code here
+    rectange=[m*"*" for _ in range(n)]
+    return rectange
+
+print(generate_rectangle(2,3))
