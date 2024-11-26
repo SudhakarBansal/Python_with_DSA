0023c0f8c8304c86bcd00b1b87e7cc48e7ab5249
Sudhakar Bansal <111216638+SudhakarBansal@users.noreply.github.com>
2024-11-26 18:20:00 +0530

    Valid Perfect Square

---
+++ 03_maths_problems/39_is_perfect_square.py
@@ -0,0 +1,24 @@
+# Valid Perfect Square
+
+def is_perfect_square(num):
+    """
+    Function to check if a number is a perfect square.
+    
+    Parameters:
+    num (int): The number to check.
+    
+    Returns:
+    bool: True if num is a perfect square, False otherwise.
+    """
+    # Your code here
+    if num<1:
+        return False
+
+    i=1
+    while(i*i <= num):
+        if i*i==num:
+            return True
+        i+=1
+    return False
+
+print(is_perfect_square(2))
