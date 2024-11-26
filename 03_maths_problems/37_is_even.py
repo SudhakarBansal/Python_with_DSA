2a759f8286771014fbc921c637c3655607d28127
Sudhakar Bansal <111216638+SudhakarBansal@users.noreply.github.com>
2024-11-26 17:24:13 +0530

    Even Number

---
+++ 03_maths_problems/37_is_even.py
@@ -0,0 +1,17 @@
+# Check for Even Number
+
+def is_even(n):
+    """
+    Function to check if a number is even.
+    
+    Parameters:
+    n (int): The number to check.
+    
+    Returns:
+    bool: True if n is even, False otherwise.
+    """
+    # Your code here
+    return (n%2==0)
+
+
+print(is_even(7))
