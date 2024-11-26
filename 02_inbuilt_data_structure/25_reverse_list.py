402940dee345fea5461f1b01d683d0d05bb69278
Sudhakar Bansal <111216638+SudhakarBansal@users.noreply.github.com>
2024-11-26 12:36:24 +0530

    Reverse a List

---
+++ 02_inbuilt_data_structure/25_reverse_list.py
@@ -0,0 +1,10 @@
+# Program to Reverse a List
+
+def reverse_list(lst):
+    start = 0
+    end = len(lst) - 1
+    while start < end:
+        lst[start], lst[end] = lst[end], lst[start]
+        start += 1
+        end -= 1
+    return lst
