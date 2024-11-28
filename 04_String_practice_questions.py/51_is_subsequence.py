5d8224b03f1c0d04f53ea8badc7109798991858d
Sudhakar Bansal <111216638+SudhakarBansal@users.noreply.github.com>
2024-11-28 17:34:24 +0530

    Check Subsequence

---
+++ 04_String_practice_questions.py/51_is_subsequence.py
@@ -0,0 +1,17 @@
+# Check Subsequence
+
+# Input: s = "abcde", t = "ace"
+# Output: True
+ 
+# Input: s = "abcde", t = "aec"
+# Output: False
+def is_subsequence(s: str, t: str) -> bool:
+    i, j = 0, 0
+    while i < len(s) and j < len(t):
+        if s[i] == t[j]:
+            i += 1
+        j += 1
+    return i == len(s)    
+
+
+print(is_subsequence("aaaaaa","bbaaaa"))
